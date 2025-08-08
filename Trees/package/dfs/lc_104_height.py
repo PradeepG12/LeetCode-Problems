44597e11c3ecbd0dc4995cd54f30c971a65ebd84
Pradeep <pg649599@gmail.com>
2025-08-08 11:55:21 +0530

    Tree - IsBalanced and Diameter

--- Trees/package/dfs/lc_104_height.py
+++ Trees/package/dfs/lc_104_height.py
@@ -6,3 +6,39 @@ def maxDepth(root: Optional[TreeNode]) -> int:
     if not root:
         return 0
     return max(maxDepth(root.left), maxDepth(root.right)) + 1
+
+def isBalancedBrute(root: Optional[TreeNode]) -> bool:
+    def __helper(root):
+        if not root:
+            return 0, True
+        l_height, l_balanced = __helper(root.left)
+        r_height, r_balanced = __helper(root.right)
+        if not l_balanced or not r_balanced:
+            return 0, False
+        if abs(l_height - r_height) > 1:
+            return 0, False
+        return __helper(max(l_height, r_height)+1, True)
+    _, result = __helper(root)
+    return result
+
+def isBalanced(root: Optional[TreeNode]) -> bool:
+    def _helper(root):
+        if not root: return 0
+        left = _helper(root.left)
+        right = _helper(root.right)
+        if (left or right) == -1: return -1
+        if abs(left-right)>1: return -1
+        return max(left, right) + 1
+    return _helper(root) != -1
+
+def diameterOfBinaryTree(root: Optional[TreeNode]) -> int:
+    maxi = 0
+    def _helper(root):
+        nonlocal maxi
+        if not root: return 0
+        left = _helper(root.left)
+        right = _helper(root.right)
+        maxi = max(left+right, maxi)
+        return max(left, right)+1
+    _helper(root)
+    return maxi
